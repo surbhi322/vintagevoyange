9a448f4a763732109a76d1716118545dc9d68dea
Surbhi Lande <slande@texala.net>
2024-05-21 16:58:45 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Banner from './pages/Banner';
+
+function App() {
+  return (
+    <>
+    <Banner />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Banner.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+const Banner = () => {
+    return (
+        <Container fluid>
+            <div className='banner_outer'>
+                <div className='inside_div'>
+                    <div></div>
+                </div>
+            </div>
+        </Container>
+    )
+}
+
+export default Banner
